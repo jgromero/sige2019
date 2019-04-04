67d12576944e1ac9b5c38453a322199169d4a01c
Juan Gómez Romero <jgomez@decsai.ugr.es>
2019-04-04 20:08:27 +0200

    Update credit-clase.R

--- prácticas/03. Análisis predictivo/credit-clase.R
+++ prácticas/03. Análisis predictivo/credit-clase.R
@@ -12,3 +12,26 @@ glimpse(data)
 df_status(data)
 ggplot(data) + geom_histogram(aes(x = Class, fill = Class), stat = 'count')
 
+## Crear modelo de predicción usando rpart
+# Particiones entrenamiento / test
+trainIndex <- createDataPartition(data$Class, p = .75, list = FALSE, times = 1)
+train <- data[ trainIndex, ] 
+val   <- data[-trainIndex, ]
+
+# Entrenar modelo
+rpartCtrl <- trainControl(
+  verboseIter = F, 
+  classProbs = TRUE, 
+  summaryFunction = twoClassSummary)
+rpartParametersGrid <- expand.grid(
+  .cp = c(0.001, 0.01, 0.1, 0.5))
+rpartModel <- train(
+  Class ~ ., 
+  data = train, 
+  method = "rpart", 
+  metric = "ROC", 
+  trControl = rpartCtrl, 
+  tuneGrid = rpartParametersGrid)
+print(rpartModel)
+
+
